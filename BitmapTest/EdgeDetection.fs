730de4540e30fd540fb5f55075272e7cd9cc2382
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-01 14:45:53 +0100

    first commit

---
+++ BitmapTest/EdgeDetection.fs
@@ -0,0 +1,64 @@
+﻿module BitmapTest.EdgeDetection
+
+open ImageProcessor
+open ImageProcessor.Imaging.Filters.EdgeDetection
+
+let makeFactory() =
+    (new ImageFactory())
+        .Load(__SOURCE_DIRECTORY__ + "/small_rec.jpg")
+
+let edgeDetection (factory: ImageFactory) =
+    factory
+        .Reset()
+        .DetectEdges(new Laplacian3X3EdgeFilter())
+        .Image
+        .Save("laplacian3x3.jpg")
+        
+    factory
+        .Reset()
+        .DetectEdges(new KayyaliEdgeFilter())
+        .Image
+        .Save("kayyali.jpg")
+
+    factory
+        .Reset()
+        .DetectEdges(new KirschEdgeFilter())
+        .Image
+        .Save("kirsch.jpg")
+
+    factory
+        .Reset()
+        .DetectEdges(new PrewittEdgeFilter())
+        .Image
+        .Save("prewitt.jpg")
+
+    factory
+        .Reset()
+        .DetectEdges(new RobertsCrossEdgeFilter())
+        .Image
+        .Save("robertscross.jpg")
+
+    factory
+        .Reset()
+        .DetectEdges(new ScharrEdgeFilter())
+        .Image
+        .Save("scharr.jpg")
+
+    factory
+        .Reset()
+        .DetectEdges(new SobelEdgeFilter())
+        .Image
+        .Save("sobel.jpg")
+    
+    factory
+        .Reset()
+        .DetectEdges(new Laplacian5X5EdgeFilter())
+        .Image
+        .Save("laplacian5x5.jpg")
+    
+    factory
+        .Reset()
+        .DetectEdges(new LaplacianOfGaussianEdgeFilter())
+        .Image
+        .Save("laplacianGauss.jpg")
+    
--- BitmapTest/Program.fs
+++ BitmapTest/Program.fs
@@ -1,7 +1,67 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module BitmapTest.Root
+
+open System
+open System.Drawing
+open System.Drawing.Imaging
+
+let redraw setAttributes (img: Image) =
+    let newImg = new Bitmap(img.Width, img.Height)
+    use g = Graphics.FromImage newImg
+    g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, setAttributes <| new ImageAttributes())
+    newImg
+
+let grayscale: (Image -> Bitmap) =
+    redraw
+        (fun attrs ->
+            attrs.SetColorMatrix(
+                new ColorMatrix(
+                    [| [|  0.3f;  0.3f;  0.3f;  0.f;  0.f |]
+                       [| 0.59f; 0.59f; 0.59f;  0.f;  0.f |]
+                       [| 0.11f; 0.11f; 0.11f;  0.f;  0.f |]
+                       [|   0.f;   0.f;   0.f;  1.f;  0.f |]
+                       [|   0.f;   0.f;   0.f;  0.f;  1.f |] |]))
+            attrs)
+
+let blackAndWhite: (Image -> Bitmap) =
+    redraw
+        (fun attrs ->
+            attrs.SetColorMatrix(
+                new ColorMatrix(
+                    [| [|  1.5f;  1.5f;  1.5f;  0.f;  0.f |]
+                       [|  1.5f;  1.5f;  1.5f;  0.f;  0.f |]
+                       [|  1.5f;  1.5f;  1.5f;  0.f;  0.f |]
+                       [|   0.f;   0.f;   0.f;  1.f;  0.f |]
+                       [|  -1.f;  -1.f;  -1.f;  0.f;  1.f |] |]))
+            attrs)
+
+let invert: (Image -> Bitmap) =
+    redraw
+        (fun attrs ->
+            attrs.SetColorMatrix(
+                new ColorMatrix(
+                    [| [|  -1.f;   0.f;  0.f;  0.f;  0.f |]
+                       [|   0.f;  -1.f;  0.f;  0.f;  0.f |]
+                       [|   0.f;   0.f; -1.f;  0.f;  0.f |]
+                       [|   0.f;   0.f;  0.f;  1.f;  0.f |]
+                       [|   1.f;   1.f;  1.f;  0.f;  1.f |] |]))
+            attrs)
+
+
+let threshold th =
+    redraw (fun attrs -> attrs.SetThreshold th; attrs)
+
+let loadBitmap filePath =
+    Bitmap.FromFile filePath
+
+let save filePath (image: Image) =
+    image.Save filePath
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+
+    __SOURCE_DIRECTORY__ + "/small_rec.jpg"
+    |> loadBitmap
+    |> blackAndWhite 
+    |> save "bnw.jpg"
+
+    0
