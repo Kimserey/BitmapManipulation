f285405ba31554afe99631dddc89022eb03196a2
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-04 00:13:28 +0100

    fully working example

--- BitmapTest/Program.fs
+++ BitmapTest/Program.fs
@@ -1,69 +1,24 @@
 ﻿module BitmapTest.Root
 
 open System
-open System.Drawing
-open System.Drawing.Imaging
 open FredsImageMagickScripts
-
-let saveToTiff (filName: string) (img: Image) =
-    img.Save(filName, ImageFormat.Tiff)
-
-let redraw setAttributes (img: Image) =
-    let newImg = new Bitmap(img.Width, img.Height)
-    use g = Graphics.FromImage newImg
-    g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, setAttributes <| new ImageAttributes())
-    newImg
-
-let grayscale: (Image -> Bitmap) =
-    redraw
-        (fun attrs ->
-            attrs.SetColorMatrix(
-                new ColorMatrix(
-                    [| [|  0.3f;  0.3f;  0.3f;  0.f;  0.f |]
-                       [| 0.59f; 0.59f; 0.59f;  0.f;  0.f |]
-                       [| 0.11f; 0.11f; 0.11f;  0.f;  0.f |]
-                       [|   0.f;   0.f;   0.f;  1.f;  0.f |]
-                       [|   0.f;   0.f;   0.f;  0.f;  1.f |] |]))
-            attrs)
-
-let blackAndWhite: (Image -> Bitmap) =
-    redraw
-        (fun attrs ->
-            attrs.SetColorMatrix(
-                new ColorMatrix(
-                    [| [|  1.5f;  1.5f;  1.5f;  0.f;  0.f |]
-                       [|  1.5f;  1.5f;  1.5f;  0.f;  0.f |]
-                       [|  1.5f;  1.5f;  1.5f;  0.f;  0.f |]
-                       [|   0.f;   0.f;   0.f;  1.f;  0.f |]
-                       [|  -1.f;  -1.f;  -1.f;  0.f;  1.f |] |]))
-            attrs)
-
-let invert: (Image -> Bitmap) =
-    redraw
-        (fun attrs ->
-            attrs.SetColorMatrix(
-                new ColorMatrix(
-                    [| [|  -1.f;   0.f;  0.f;  0.f;  0.f |]
-                       [|   0.f;  -1.f;  0.f;  0.f;  0.f |]
-                       [|   0.f;   0.f; -1.f;  0.f;  0.f |]
-                       [|   0.f;   0.f;  0.f;  1.f;  0.f |]
-                       [|   1.f;   1.f;  1.f;  0.f;  1.f |] |]))
-            attrs)
-
-
-let threshold th =
-    redraw (fun attrs -> attrs.SetThreshold th; attrs)
-
-let loadBitmap filePath =
-    Bitmap.FromFile filePath
-
-let save filePath (image: Image) =
-    image.Save filePath
+open ImageMagick
+open Tesseract
 
 [<EntryPoint>]
 let main argv = 
+    use img = new MagickImage(__SOURCE_DIRECTORY__ + "/original.jpg")
+    let cleaner = TextCleanerScript()
+    cleaner.FilterOffset <- new Percentage(9.1)
+    cleaner.Trim <- true
+    cleaner.Execute(img).Write("temp.jpg")
 
-    Bitmap.FromFile @"C:\Users\Kimserey Lam\Desktop\serak\TrainData\eng2.LobsterTwo.exp0.png"
-    |> saveToTiff @"C:\Users\Kimserey Lam\Desktop\serak\TrainData\eng2.LobsterTwo.exp0.tiff"
+    use engine = new TesseractEngine(__SOURCE_DIRECTORY__ + "/tessdata", "eng")
+    use img = Pix.LoadFromFile("temp.jpg")
+    use page = engine.Process img
 
+    printfn "%s" (page.GetText())
+    printfn "Confidence: %.4f" (page.GetMeanConfidence())
+
+    Console.ReadKey() |> ignore
     0
